68be0df957870baf9fba8aeb09aa0bb5ae53a64d
miguelaraujo97 <miguelaraujo9797@gmail.com>
2022-06-14 22:03:16 +0100

    Deleted old frontend react app.

--- FrontEnd/routing/routing/src/Reducer.js
+++
@@ -1,3 +0,0 @@
-function reducer(state, action){
-    
-}
--- FrontEnd/routing/routing/src/context/reducers/ProjectReducer.jsx
+++
@@ -1,5 +0,0 @@
-import {
-    FETCH_PROJECTS_STARTED,
-    FETCH_PROJECTS_SUCCESS,
-    FETCH_PROJECTS_FAILURE
-  } from '../actions/ProjectActions'
